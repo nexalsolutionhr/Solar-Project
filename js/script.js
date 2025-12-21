d648bb4f4c3794b4391c626eeffce9f2c0fd9340
prince-kr-dev <princekumark2004@gmail.com>
2025-12-21 15:49:52 +0530

    Added image gallery Carousel in work page

--- js/script.js
+++ js/script.js
@@ -31,3 +31,39 @@ accordionHeaders.forEach(header => {
     });
   });
 });
+
+function scrollGallery(direction) {
+  const gallery = document.getElementById("galleryScroll");
+  const scrollAmount = 380;
+  gallery.scrollBy({
+    left: direction * scrollAmount,
+    behavior: "smooth"
+  });
+}
+
+
+const lightbox = document.getElementById("imageLightbox");
+const lightboxImg = document.getElementById("lightboxImg");
+const closeBtn = document.querySelector(".lightbox-close");
+
+// Open image on image click
+document.querySelectorAll(".gallery-scroll img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.classList.add("show");
+    lightboxImg.src = img.src;
+  });
+});
+
+// Close on click
+lightbox.addEventListener("click", (e) => {
+  if (e.target !== lightboxImg) {
+    lightbox.classList.remove("show");
+  }
+});
+
+// Close on ESC key
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    lightbox.classList.remove("show");
+  }
+});
